65b7a94cf87fba56556aafd361f6897aee3b57ac
rimever <mot_siva_ordin@hotmail.com>
2019-04-01 19:54:46 +0900

    自己完結型で出力した内容も含めてコミット

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".NET Core 2.1で開発されたコンソールアプリケーションです。");
+            Console.ReadKey();
+        }
+    }
+}
